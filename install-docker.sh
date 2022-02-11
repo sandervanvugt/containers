a6c66688029df82efd125031e228db756eae260d
sandervanvugt <mail@sandervanvugt.nl>
2022-02-11 11:26:22 +0100

    message

---
+++ install-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# this script was developed on Ubuntu LTS 20.04 and is not tested on other distributions
+
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+echo Docker is now installed
